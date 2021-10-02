d4d609f7ffb0d5e815f6fb475afbd86af991fec1
ruanzh <rzh0001@qq.com>
2021-10-02 16:40:46 +0800

    fix

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/demo/eth_hub/service/IEtherPayoutService.java
@@ -0,0 +1,13 @@
+package org.jeecg.modules.demo.eth_hub.service;
+
+import org.jeecg.modules.demo.eth_hub.entity.EtherPayout;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: ether_payout
+ * @Author: jeecg-boot
+ * @Version: V1.0
+ */
+public interface IEtherPayoutService extends IService<EtherPayout> {
+
+}
---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/demo/eth_hub/service/impl/EtherPayoutServiceImpl.java
@@ -0,0 +1,18 @@
+package org.jeecg.modules.demo.eth_hub.service.impl;
+
+import org.jeecg.modules.demo.eth_hub.entity.EtherPayout;
+import org.jeecg.modules.demo.eth_hub.mapper.EtherPayoutMapper;
+import org.jeecg.modules.demo.eth_hub.service.IEtherPayoutService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: ether_payout
+ * @Author: jeecg-boot
+ * @Version: V1.0
+ */
+@Service
+public class EtherPayoutServiceImpl extends ServiceImpl<EtherPayoutMapper, EtherPayout> implements IEtherPayoutService {
+
+}
